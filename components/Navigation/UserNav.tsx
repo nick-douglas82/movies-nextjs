cef673babc5e02dd55c73b932f7c28d8a6faec76
Nick Douglas <nick.douglas82@gmail.com>
2024-01-26 12:51:10 +0000

    Image fix

--- components/Navigation/UserNav.tsx
+++ components/Navigation/UserNav.tsx
@@ -3,7 +3,7 @@ import { NavItem } from '@/components/Navigation/NavItem'
 export const UserNav = () => {
   const authenticated = false
   return (
-    <nav className="mb-10 mt-5 flex w-full justify-end space-x-5 text-right">
+    <nav className="mr-10 flex w-full justify-end space-x-5 text-right">
       <NavItem href="/login" condition={!authenticated}>
         Login
       </NavItem>
--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -5,10 +5,10 @@ import { SiteLogo } from '@/components/SiteLogo'
 
 export const SiteHeader = () => {
   return (
-    <header className="absolute z-50 flex items-center gap-x-10 p-4">
+    <header className="absolute z-50 flex w-full items-center gap-x-10 p-4">
       <SiteLogo />
       <SiteNav />
-      {/* <UserNav /> */}
+      <UserNav />
     </header>
   )
 }
